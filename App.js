0abbfdbbae68023185dddae71e06963839db91ac
saitejam12 <saitejamuddana@gmail.com>
2019-09-19 00:18:58 -0500

    first change of the project.
    Checking the linting and other settings for the native project

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>it changed...now what?</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
